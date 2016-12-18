941235324f0cfbdc855dbe34c99ddcecff112383
gliechty <guy.liechty@gmail.com>
2016-12-17 17:39:10 -0700

    finished exercise 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,36 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+// app.value('start_date', '10/31/2016');
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+app.controller("WelcomeController", WelcomeController);
+
+function WelcomeController(){
+  this.full_name = "Guy Liechty";
+  this.age = 26;
+  this.city = "Denver";
+  this.state="Colorado";
+  this.shout = function(input){
+  		input = input || '';
+		var out = input.toUpperCase();
+		return out + "!";
+	};
+}
+
+app.controller("ClassController", ClassController);
+
+function ClassController(){
+	this.class_name = "Big Nick's Cult of Zebulon Online";
+	this.enrolled_students = 10;
+	this.start_date = '10/31/2016';
+	this.end_date = '02/02/2017';
+	this.daysRemaining = function(){
+		var oneDay = 24*60*60*1000
+		var current = new Date();
+		var end = new Date(2017,1,2);
+		console.log(current);
+		console.log(end);
+		var daysLeft = Math.round(Math.abs((current.getTime()-end.getTime())/(oneDay))+1);
+		return daysLeft;
+	};
+}
