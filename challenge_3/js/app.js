192c36be6644d575d6051e489917b1925ade9f66
gliechty <guy.liechty@gmail.com>
2016-12-19 09:00:46 -0700

    saving as of Monday morning

--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,23 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController);
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+}
+
+app.controller("loanController", loanController);
+
+function loanController(){
+	// this.amount = 1;
+	// this.apr = 5;
+	// this.month= function(){
+	// 	month = [];
+	// 	for (i=0; i<100; i++){
+	// 		month++;
+	// 		return month;
+	// 	}
+	// };
+}
